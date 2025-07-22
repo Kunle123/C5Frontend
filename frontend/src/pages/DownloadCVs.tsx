015bb124d36f037a27118638c4258f94d4578fe2
Kunle Dev <Kunle2000@gmail.com>
2025-07-22 18:14:21 +0100

    feat: replace DownloadCVs page with new Tailwind-styled CVDownload design from Zen project

--- frontend/src/pages/DownloadCVs.tsx
+++ frontend/src/pages/DownloadCVs.tsx
@@ -1,21 +1,5 @@
-import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
-import PreviousDocumentsList from '../components/PreviousDocumentsList';
+import { CVDownload } from "@/components/CVDownload";
 
-const DownloadCVs: React.FC = () => {
-  const token = localStorage.getItem('token') || '';
-
-  return (
-    <Box py={8} maxW="800px" mx="auto">
-      <Heading as="h2" size="xl" fontWeight={800} mb={6} textAlign="center" color="brand.700">
-        Download Your CVs & Cover Letters
-      </Heading>
-      <Text mb={8} textAlign="center" color="gray.600" fontSize="lg">
-        Download your previously generated CVs and cover letters as DOCX files.
-      </Text>
-      <PreviousDocumentsList token={token} />
-    </Box>
-  );
-};
-
-export default DownloadCVs; 
+export default function DownloadCVs() {
+  return <CVDownload />;
+} 
